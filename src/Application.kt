e7f43487094bc699bf944f3a240e764d40e889da
Chris Harrington <ceharrin@icloud.com>
2019-05-11 17:16:56 -0500

    Initial Commit

---
+++ src/Application.kt
@@ -0,0 +1,25 @@
+package com.example
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/demo") {
+            call.respondText("Hello World Demo!", contentType = ContentType.Text.Plain)
+        }
+    }
+}
+
